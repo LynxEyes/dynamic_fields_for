6d05c97517e7cbb35cd9a150418a1ebf258fdc7f
Ivo Jesus <ivo.jesus@gmail.com>
2013-06-11 23:13:36 +0100

    Made the DynamicFieldsFor a Rails Engine to make the assets auto-loadable on Rails! Restructured the assets..

--- lib/dynamic_fields_for.rb
+++ lib/dynamic_fields_for.rb
@@ -3,3 +3,7 @@ require 'rails'
 # -----------------------------------------------------------------------------
 require 'dynamic_fields_for/form_builder_additions'
 # -----------------------------------------------------------------------------
+module DynamicFieldsFor
+  class DynamicFieldsForEngine < Rails::Engine
+  end
+end
